bcc8d65da4919142dc77926d9c3a3d77cad90a71
michalszczepanski <michalszczepanski07@gmail.com>
2026-08-28 15:24:14 +0200

    AddDocumentTransformer

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,9 +1,11 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.OpenApi;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 using Scalar.AspNetCore;
 using WebApplication1.Data;
 using WebApplication1.Models;
+using static Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
 //add services to the container
@@ -13,14 +15,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
-
-var key = builder.Configuration.GetValue<string>("AppSettings:Secret");
+//-----
+var key = builder.Configuration.GetValue<string>("AppSettings:Secret"); //get the secret key from appsettings.json
 builder.Services.AddAuthentication((u =>
         {
-            u.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            u.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            u.DefaultAuthenticateScheme = AuthenticationScheme;
+            u.DefaultChallengeScheme = AuthenticationScheme;
         }
-    )).AddJwtBearer( u =>
+    )).AddJwtBearer( u => //configure JWT Bearer authentication!!!
     {
         u.RequireHttpsMetadata = false;
         u.SaveToken = true;
@@ -29,10 +31,11 @@ builder.Services.AddAuthentication((u =>
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(key)),
             ValidateIssuer = false,
-            ValidateAudience = false
+            ValidateAudience = false,
+            ValidateLifetime = true
         };
     });
-
+//-----
 builder.Services.AddOpenApi();
 var app = builder.Build();
 //configure http request pipeline
@@ -51,3 +54,28 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+internal sealed class BarerSeciuritySchemeTransformer(
+    Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider authenticationSchemeProvider)
+    : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context,
+        CancellationToken cancellationToken)
+    {
+        var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+        if (authenticationSchemes.Any(s => s.Name == AuthenticationScheme))
+        {
+            var requirement = new Dictionary<string, IOpenApiSecurityScheme>
+            {
+                
+                    [AuthenticationScheme] = new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer",
+                        BearerFormat = "JWT",
+                    }
+                
+            };
+        }
+    }
+}
