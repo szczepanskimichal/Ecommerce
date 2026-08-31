8d77bad3da2ca20137e1d0064fa54abce6d224d1
michalszczepanski <michalszczepanski07@gmail.com>
2026-08-31 16:42:22 +0200

    working with images in menuItem

--- EccomerceFrontEnd/src/views/menuItem/MenuItemList.vue
+++ EccomerceFrontEnd/src/views/menuItem/MenuItemList.vue
@@ -1,8 +1,12 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 import menuItemService from '@/services/menuItemService.js'
+import { CONFIG_API_IMAGE_URL } from '@/constans/config.js'
+import { APP_ROUTE_NAMES } from '@/constans/routeNames.js'
+import { useRouter } from 'vue-router'
 
-const menuItems = ref([])
+const router = useRouter()
+const menuItems = reactive([])
 const loading = ref(false)
 const errorMessage = ref('')
 
@@ -10,8 +14,9 @@ const fetchMenuItems = async () => {
   loading.value = true
   errorMessage.value = ''
   try {
-    menuItems.value = await menuItemService.getMenuItems()
-    console.log(menuItems.value)
+    var result = await menuItemService.getMenuItems()
+    menuItems.push(...result)
+    //console.log(menuItems)
   } catch (error) {
     console.error('Error fetching menu items:', error)
     errorMessage.value =
@@ -25,10 +30,95 @@ onMounted(() => {
 })
 </script>
 <template>
-  <h1>Menu Item List</h1>
-  <div v-if="loading">Loading...</div>
-  <div v-else-if="errorMessage" class="alert alert-warning" role="alert">{{ errorMessage }}</div>
-  <ul v-else>
-    <li v-for="item in menuItems" :key="item.id">{{ item.name }}</li>
-  </ul>
+  <div class="container px-3">
+    <div v-if="loading" class="d-flex justify-content-center align-items-center vh-100">
+      <div class="spinner-grow text-success" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+
+    <div v-else>
+      <div>
+        <div
+          class="card-header d-flex flex-column flex-md-row justify-content-between align-items-md-center p-3"
+        >
+          <div>
+            <h2 class="h5 text-success">Menu Items</h2>
+            <p class="mb-0 text-muted small">Manage your restaurant's offerings</p>
+          </div>
+          <button
+            class="btn btn-success btn-sm gap-2 rounded-1 px-4 py-2"
+            @click="router.push({ name: APP_ROUTE_NAMES.CREATE_MENU_ITEM })"
+          >
+            <i class="bi bi-plus-square"></i> &nbsp;
+            <span>Add Item</span>
+          </button>
+        </div>
+        <div class="card-body p-3">
+          <div class="table-responsive">
+            <table class="table table-hover align-middle mb-0">
+              <thead>
+                <tr>
+                  <th class="ps-3 small text-muted">Item</th>
+                  <th class="small text-muted">Category</th>
+                  <th class="small text-muted">Price</th>
+                  <th class="small text-muted">Tag</th>
+                  <th class="pe-3 text-end small text-muted">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="menuItem in menuItems" :key="menuItem.id">
+                  <td class="ps-3">
+                    <div class="d-flex align-items-center">
+                      <img
+                        :src="CONFIG_API_IMAGE_URL + menuItem.image"
+                        alt="Item"
+                        class="rounded object-fit-cover me-2"
+                        style="width: 50px; height: 50px"
+                      />
+                      <div>
+                        <div class="fw-semibold small">{{ menuItem.name }}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <span class="badge bg-success bg-opacity-10 text-success small">
+                      {{ menuItem.category }}
+                    </span>
+                  </td>
+                  <td class="fw-semibold small">${{ menuItem.price.toFixed(2) }}</td>
+                  <td>
+                    <span class="badge bg-info bg-opacity-10 text-info small">
+                      {{ menuItem.specialTag }}
+                    </span>
+                  </td>
+                  <td class="pe-3 text-end">
+                    <div class="d-flex gap-2 justify-content-end">
+                      <button
+                        class="btn btn-sm btn-outline-success"
+                        @click="
+                          router.push({
+                            name: APP_ROUTE_NAMES.EDIT_MENU_ITEM,
+                            params: { id: menuItem.id },
+                          })
+                        "
+                      >
+                        <i class="bi bi-pencil-square"></i>
+                      </button>
+                      <button
+                        class="btn btn-sm btn-outline-danger"
+                        @click="handleMenuItemDelete(menuItem.id)"
+                      >
+                        <i class="bi bi-trash3-fill"></i>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
